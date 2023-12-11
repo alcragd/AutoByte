5bb0973f80338193ee4b0533ccbd2bcbd091ebcc
alcragd <>
2023-12-10 21:30:01 -0600

    latest database

--- autobyte.sql
+++ autobyte.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-12-2023 a las 19:45:51
--- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.2.0
+-- Tiempo de generación: 11-12-2023 a las 04:28:22
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,15 +31,17 @@ CREATE TABLE `almacen` (
   `idproducto` int(11) NOT NULL,
   `nombre` varchar(45) NOT NULL,
   `stock` int(11) NOT NULL,
-  `costo` float NOT NULL
+  `costo` float NOT NULL,
+  `type` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `almacen`
 --
 
-INSERT INTO `almacen` (`idproducto`, `nombre`, `stock`, `costo`) VALUES
-(1, 'Nissan Versa', 50, 10000);
+INSERT INTO `almacen` (`idproducto`, `nombre`, `stock`, `costo`, `type`) VALUES
+(1, 'Nissan Versa', 50, 10000, 0),
+(2, 'NEUMATICOS', 70, 50, 1);
 
 -- --------------------------------------------------------
 
@@ -51,7 +53,11 @@ CREATE TABLE `cuentas` (
   `idcuenta` int(11) NOT NULL,
   `user` text NOT NULL,
   `pass` text NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apellidos` varchar(45) NOT NULL,
+  `numero` int(11) NOT NULL,
   `email` varchar(45) NOT NULL,
+  `direccion` text NOT NULL,
   `type` int(11) NOT NULL DEFAULT 3
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -59,11 +65,8 @@ CREATE TABLE `cuentas` (
 -- Volcado de datos para la tabla `cuentas`
 --
 
-INSERT INTO `cuentas` (`idcuenta`, `user`, `pass`, `email`, `type`) VALUES
-(5, 'zoe', '123', 'a@a.a', 3),
-(6, 'a', 'a', 'a@a.a', 3),
-(7, '', '', '', 3),
-(8, 'josuegay', '123', 'putojosue@gay.com', 3);
+INSERT INTO `cuentas` (`idcuenta`, `user`, `pass`, `nombre`, `apellidos`, `numero`, `email`, `direccion`, `type`) VALUES
+(9, 'a', 'a', 'a', 'a', 55, 'a', 'a', 3);
 
 --
 -- Índices para tablas volcadas
@@ -90,13 +93,13 @@ ALTER TABLE `cuentas`
 -- AUTO_INCREMENT de la tabla `almacen`
 --
 ALTER TABLE `almacen`
-  MODIFY `idproducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idproducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `cuentas`
 --
 ALTER TABLE `cuentas`
-  MODIFY `idcuenta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `idcuenta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
